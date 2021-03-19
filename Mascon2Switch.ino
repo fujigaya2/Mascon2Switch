2f71cefda94f3c28efc8a80f02fbd62ee06c3d7f
fujigaya2 <66097344+fujigaya2@users.noreply.github.com>
2021-03-19 11:39:27 +0900

    修正
    ボタン配置、マスコンのノッチのパラメータを修正

--- Mascon2Switch.ino
+++ Mascon2Switch.ino
@@ -8,18 +8,10 @@
 
 Mascon Joystick;
 
-#define EEP_ACC_RATE_DEFAULT 25
-#define EEP_DEC_RATE_DEFAULT 15
-#define EEP_COAST_RATE_DEFAULT 5
 
 //USB SERIAL DEBUGをする場合はコメントアウトを取る
 //#define USB_SERIAL_ENABLE
 
-int joy_gear_mode = GEAR_MODE_SPD;//EEP_CONTROL_DEFAULT
-int joy_accrate = EEP_ACC_RATE_DEFAULT;//50msecでどのくらい加速するか？２０４８が１００％の基準
-int  joy_decrate = EEP_DEC_RATE_DEFAULT;
-int  joy_coastrate = EEP_COAST_RATE_DEFAULT;
-int prev_speed = 0;
 unsigned int aPsxData = 0;
 int gPreviousL6_wait = 50;
 
@@ -31,6 +23,31 @@ unsigned long gPreviousL6 = 250; //   50ms interval
 
 char aText[16 + 1];
 
+void button_release()
+{
+  //全ボタンリリース(Left_Stick以外）
+  SwitchControlLibrary().releaseButton(Button::Y);
+  SwitchControlLibrary().releaseButton(Button::B);
+  SwitchControlLibrary().releaseButton(Button::A);
+  SwitchControlLibrary().releaseButton(Button::X);
+  SwitchControlLibrary().releaseButton(Button::L);
+  SwitchControlLibrary().releaseButton(Button::R);
+  SwitchControlLibrary().releaseButton(Button::ZL);
+  SwitchControlLibrary().releaseButton(Button::ZR);
+  SwitchControlLibrary().releaseButton(Button::MINUS);
+  SwitchControlLibrary().releaseButton(Button::PLUS);
+  SwitchControlLibrary().releaseButton(Button::LCLICK);
+  SwitchControlLibrary().releaseButton(Button::RCLICK);
+  SwitchControlLibrary().releaseButton(Button::HOME);
+  SwitchControlLibrary().releaseButton(Button::CAPTURE);
+  SwitchControlLibrary().releaseHatButton(HatButton::UP);//これで良いか？
+  SwitchControlLibrary().releaseHatButton(HatButton::RIGHT);//これで良いか？
+  SwitchControlLibrary().releaseHatButton(HatButton::DOWN);//これで良いか？
+  SwitchControlLibrary().releaseHatButton(HatButton::LEFT);//これで良いか？
+  
+}
+
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -41,6 +58,10 @@ void setup()
   Serial.println("Serial Begin.");
 #endif
   SwitchControlLibrary();
+
+  button_release();
+  SwitchControlLibrary().moveRightStick(128, 128);
+
 }
 
 void loop()
@@ -58,35 +79,18 @@ void loop()
     //Read ButtonStatus and continueously push count    
     Joystick.GetButtonStatus(aPsxData);
     //Speed　→mode変えても変わらない？ 
-    Joystick.Interval(joy_gear_mode ,joy_accrate,joy_decrate ,joy_coastrate );
+    //Joystick.Interval(joy_gear_mode ,joy_accrate,joy_decrate ,joy_coastrate );
     //Reset task
     gPreviousL5 = millis();
   }
+
   
   //buttonの確認
   if( (millis() - gPreviousL6) >= gPreviousL6_wait)
   {
     gPreviousL6_wait = 50;
-    //まず全ボタンリリース(Left_Stick以外）
-    SwitchControlLibrary().releaseButton(Button::Y);
-    SwitchControlLibrary().releaseButton(Button::B);
-    SwitchControlLibrary().releaseButton(Button::A);
-    SwitchControlLibrary().releaseButton(Button::X);
-    SwitchControlLibrary().releaseButton(Button::L);
-    SwitchControlLibrary().releaseButton(Button::R);
-    SwitchControlLibrary().releaseButton(Button::ZL);
-    SwitchControlLibrary().releaseButton(Button::ZR);
-    SwitchControlLibrary().releaseButton(Button::MINUS);
-    SwitchControlLibrary().releaseButton(Button::PLUS);
-    SwitchControlLibrary().releaseButton(Button::LCLICK);
-    SwitchControlLibrary().releaseButton(Button::RCLICK);
-    SwitchControlLibrary().releaseButton(Button::HOME);
-    SwitchControlLibrary().releaseButton(Button::CAPTURE);
-    SwitchControlLibrary().releaseHatButton(HatButton::UP);//これで良いか？
-    SwitchControlLibrary().releaseHatButton(HatButton::RIGHT);//これで良いか？
-    SwitchControlLibrary().releaseHatButton(HatButton::DOWN);//これで良いか？
-    SwitchControlLibrary().releaseHatButton(HatButton::LEFT);//これで良いか？
-    SwitchControlLibrary().moveRightStick(128, 128);
+
+    button_release();
     
     //SelectはShiftフラグとする。
     bool shift_flag = false;
@@ -174,108 +178,121 @@ void loop()
     SwitchControlLibrary().sendReport();
 
     //ノッチ系の確認
-    //Left_Stickリセット
-    SwitchControlLibrary().moveLeftStick(128, 128);
 
-    switch(Joystick.GetHandleValue())
+    int handle_pos = Joystick.GetHandleValue();
+    // L,Rボタン機能を入れておく
+    if(handle_pos <= -10 && shift_flag == true)
     {
-      case -1024://緊急ブレーキ
-      case -160:
-      case -800:
-      case -40:
-      case -20:
-      case -10:
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("ZL Button On.");
-#endif
-        SwitchControlLibrary().pressButton(Button::ZL);
-        break;
-      case -8://B8
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 0 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 0);// 左スティックは上のほう
-        break;
-      case -7://B7
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 1 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 1); // 左スティックは上のほう
-        break;
-      case -6://B6
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 2 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 2); // 左スティックは上のほう
-        break;
-      case -5://B5
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 3 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 3); // 左スティックは上のほう
-        break;
-      case -4://B4
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 4 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 4); // 左スティックは上のほう
-        break;
-      case -3://B3
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 5 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 5); // 左スティックは上のほう
-        break;
-      case -2://B2
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 6 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 6); // 左スティックは上のほう
-        break;
-      case -1://B1
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 7 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 7); // 左スティックは上のほう
-        break;
-      case 0://N
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("16 * 8 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 16 * 8); // 左スティックは真ん中
-        break;
-      case 1://N
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("128 + 25 * 1 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 1); // 左スティックは下のほう
-        break;
-      case 2://N
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("128 + 25 * 2 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 2); // 左スティックは下のほう
-        break;
-      case 3://N
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("128 + 25 * 3 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 3); // 左スティックは下のほう
-        break;
-      case 4://N
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("128 + 25 * 4 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 4); // 左スティックは上のほう
-        break;
-      case 5://N
-#if defined(USB_SERIAL_ENABLE)
-        Serial.println("128 + 25 * 5 Stick On.");
-#endif
-        SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 5); // 左スティックは下のほう
-        break;
-      default:
-        break;
+        SwitchControlLibrary().pressButton(Button::R);
+    }
+    if(handle_pos >= 5 && shift_flag == true)
+    {
+        SwitchControlLibrary().pressButton(Button::L);
+    }
+
+    if(shift_flag != true)
+    {
+      switch(handle_pos)
+      {
+        case -1024://緊急ブレーキ
+        case -160:
+        case -800:
+        case -40:
+        case -20:
+        case -10:
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("ZL Button On.");
+  #endif
+          SwitchControlLibrary().pressButton(Button::ZL);
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 8);
+          break;
+        case -8://B8
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 8 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 8);// 左スティックは上のほう
+          break;
+        case -7://B7
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 7 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 7); // 左スティックは上のほう
+          break;
+        case -6://B6
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 6 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 6); // 左スティックは上のほう
+          break;
+        case -5://B5
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 5 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 5); // 左スティックは上のほう
+          break;
+        case -4://B4
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 4.5 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 4.5); // 左スティックは上のほう
+          break;
+        case -3://B3
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 4 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 4); // 左スティックは上のほう
+          break;
+        case -2://B2//調整
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 3 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 3); // 左スティックは上のほう
+          break;
+        case -1://B1//調整
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 - 16 * 2 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 - 16 * 2); // 左スティックは上のほう
+          break;
+        case 0://N
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128); // 左スティックは真ん中
+          break;
+        case 1://P1 //値調整
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 + 25 * 1.5 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 1.5); // 左スティックは下のほう
+          break;
+        case 2://P2
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 + 25 * 2 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 2); // 左スティックは下のほう
+          break;
+        case 3://P3
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 + 25 * 3 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 3); // 左スティックは下のほう
+          break;
+        case 4://P4
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 + 25 * 4 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 4); // 左スティックは上のほう
+          break;
+        case 5://P5
+  #if defined(USB_SERIAL_ENABLE)
+          Serial.println("128 + 25 * 5 Stick On.");
+  #endif
+          SwitchControlLibrary().moveLeftStick(128, 128 + 25 * 5); // 左スティックは下のほう
+          break;
+        default:
+          break;
+      }
     }
     //ノッチ部分を送信
     SwitchControlLibrary().sendReport();    
